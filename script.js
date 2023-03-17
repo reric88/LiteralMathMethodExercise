337196bced592fb20d28e97ec3ca290acf33ab04
Eric Collard <ruffyreborn@gmail.com>
2023-03-16 20:03:46 -0500

    Completed assignmenty

---
+++ script.js
@@ -0,0 +1,77 @@
+// QUESTION 1a
+let warmHugs = "Hi, I'm Olaf and I like warm hugs."
+
+
+// QUESTION 1b
+console.log("     =====QUESTION 1b=====\n", 
+warmHugs.toUpperCase(), "\n");
+
+
+// QUESTION 1c
+console.log("     =====QUESTION 1c=====\n", 
+warmHugs = warmHugs.replace("like", "love"), "\n");
+
+
+// QUESTION 2a
+let beenImpaled = "Oh, look at that. I've been impaled."
+
+
+// QUESTION 2b
+console.log("     =====QUESTION 2b=====\n", 
+beenImpaled.slice(18), "\n");
+
+// QUESTION 3a
+let dotDotDot = "..."
+
+// QUESTION 3b
+let skullBones = `I don't have a skull${dotDotDot} or bones.`
+console.log("     =====QUESTION 3b=====\n",
+skullBones, "\n");
+
+// QUESTION 4
+console.log("     =====QUESTION 4=====\n",
+ Math.PI, "\n");
+
+ // QUESTION 5
+ let randomNumber = Math.random();
+ randomNumber *= 3;
+ randomNumber = Math.ceil(randomNumber);
+ console.log("     =====QUESTION 5=====\n", 
+ randomNumber, "\n");
+ 
+// BONUS 6
+console.log("     =====BONUS 6=====\n", 
+" Let It Go!".repeat(2).trim().toUpperCase(), "\n");
+
+// BONUS 7a
+let reindeers = "Reindeers are better than people."
+
+// BONUS 7b
+console.log("     =====BONUS 7b=====\n", reindeers.replaceAll(" ", "_"), "\n");
+
+// BONUS 8
+console.log("     =====BONUS 8=====\n", Math.SQRT2, "\n");
+console.log(Math.sqrt(2), "\n");
+
+// BONUS 9
+console.log("     =====BONUS 9=====");
+let newRandomNumber = Math.floor(Math.random() * 16) +7
+console.log(newRandomNumber);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
